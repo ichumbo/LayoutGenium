866840da97e1bdd8b2d0cd94bac2e5651597252b
ichumbo <95489893+ichumbo@users.noreply.github.com>
2022-12-14 13:58:04 -0300

    alinhamento do grid

--- index.php
+++ index.php
@@ -1,50 +1,16 @@
-<?php
-    require_once "header.php";
-?>
-    <div class="container">
+    <?php require_once "header.php"; ?>
 
-    <?php 
-    require_once "sidebar.php";
-    ?>
-        <main>
+    <div class="container d-grid">
 
-            <h1>Inicío</h1>
-
-            <!--
-            <div class="date">
-                <input type="date">
-            </div>
-            -->
-
-            <div class="insights">
-
-                <div class="sales">
-                    <span class="material-symbols-sharp">other_houses</span>
-                    <div class="middle">
-                        <div class="left">
-                            <h2>Bem-vindo ao Sistema de Gestão. Utilize o menu acima para começar.</h2>
-                            <p><b>Administrador:</b> PHOCO<br>
-                                <b>Data:</b> 07/12/2022<br>
-                                <b>Acessos:</b> 2891 <br></p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="sales">
-                    <span class="material-symbols-sharp">settings</span>
-                    <div class="middle">
-                        <div class="left">
-                            <a>Acessar a configuração obrigatória do navegador para impressão dos boletos</a>
-                            
-                            <a>Carregar dados de alunos sem responsável legal, sem financeiro e sem turmas</a>
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
-
-        </main>
+    <div>
+        <?php require_once "sidebar.php"; ?>
+    </div>
+       
+    <div>
+        <?php require_once "main.php" ?>
+    </div>
         
+    <div>
         <section class="right">
 
             <article class="statistics">
---
+++ main.php
@@ -0,0 +1,38 @@
+<main>
+
+            <h1>Inicío</h1>
+
+            <!--
+            <div class="date">
+                <input type="date">
+            </div>
+            -->
+
+            <div class="insights">
+
+                <div class="sales">
+                    <span class="material-symbols-sharp">other_houses</span>
+                    <div class="middle">
+                        <div class="left">
+                            <h2>Bem-vindo ao Sistema de Gestão. Utilize o menu acima para começar.</h2>
+                            <p><b>Administrador:</b> PHOCO<br>
+                                <b>Data:</b> 07/12/2022<br>
+                                <b>Acessos:</b> 2891 <br></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="sales">
+                    <span class="material-symbols-sharp">settings</span>
+                    <div class="middle">
+                        <div class="left">
+                            <a>Acessar a configuração obrigatória do navegador para impressão dos boletos</a>
+                            
+                            <a>Carregar dados de alunos sem responsável legal, sem financeiro e sem turmas</a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+        </main>
